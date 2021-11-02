73fe7283cbda69208c0cf948aac0565c6ff60693
Jared Wright <jaredwright.ok@gmail.com>
2021-11-02 14:17:45 -0500

    Got Blend Tree Started

--- Assets/InputSys Resources/PController.cs
+++ Assets/InputSys Resources/PController.cs
@@ -11,6 +11,7 @@ public class PController : MonoBehaviour
     public float playerSpeed = 5.0f;
     public float playerJumpHeight = 5.0f;
     public float gravity = -9.81f;
+    public float playerWeight = 25f;
     public Vector3 drag;
     public Transform sphereCastCenter;
     public LayerMask ground;
@@ -20,7 +21,7 @@ public class PController : MonoBehaviour
     private PlayerInput playerInput;
     private Vector3 pVel;
     private Transform camForward;
-    private float radiusOfCast = 1.0f;
+    private float radiusOfCast = .25f;
 
     //These are to store out inputs
     private InputAction moveAction;
@@ -34,7 +35,7 @@ public class PController : MonoBehaviour
         //Set the player controller and player input here
         playerController = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
-        camForward = GameObject.Find("ThirdPersonCamera").GetComponent<Transform>();
+        camForward = GameObject.Find("Main Camera").GetComponent<Transform>();
 
         moveAction = playerInput.actions["BasicMove"];
         jumpAction = playerInput.actions["Jump"];
@@ -44,30 +45,41 @@ public class PController : MonoBehaviour
     void Update()
     {
         Vector3 _velocity = Vector3.zero;
+        Vector3 moveVal = Vector3.zero;
+        Vector2 inputGrab;
 
-        grounded = Physics.CheckSphere(sphereCastCenter.position, radiusOfCast, ground, QueryTriggerInteraction.Ignore);
         if(grounded && _velocity.y < 0.0f)
             _velocity.y = 0.0f;
         
         //Get InputSystem input, as pulled through the Player Input Component
-        Vector2 inputGrab = moveAction.ReadValue<Vector2>();
-        
-        //Since this controls our Forward, Backward, Left and Right movements, we get it as a Vector2,
-        //then we put the x and the y into the Vector3 in the x and z slots so that we can use the
-        //PlayerController move method.
-        Vector3 moveVal = new Vector3(inputGrab.x, 0.0f, inputGrab.y);
+        inputGrab = moveAction.ReadValue<Vector2>();
+        if(!grounded)
+        {
+            Vector3 tempGrab = inputGrab;
+            tempGrab.x /= 1 + drag.x * Time.deltaTime;
+            tempGrab.y /= 1 + drag.y * Time.deltaTime;
+            moveVal = new Vector3(tempGrab.x, 0.0f, tempGrab.y);
+        }
+        else
+            moveVal = new Vector3(inputGrab.x, 0.0f, inputGrab.y);
+        //transform.Rotate(0f, camForward.rotation.y, 0f);
 
         playerController.Move(moveVal * Time.deltaTime * playerSpeed);
 
         if(jumpAction.triggered && grounded)
             _velocity.y += Mathf.Sqrt(playerJumpHeight * -3f * gravity);
 
-        _velocity.y += gravity * Time.deltaTime;
+        _velocity.y += gravity * playerWeight * Time.deltaTime;
 
         _velocity.x /= 1 + drag.x * Time.deltaTime;
         _velocity.y /= 1 + drag.y * Time.deltaTime;
         _velocity.z /= 1 + drag.z * Time.deltaTime;
         
         playerController.Move(_velocity * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics.CheckSphere(sphereCastCenter.position, radiusOfCast, ground, QueryTriggerInteraction.Ignore);        
     }
 }
